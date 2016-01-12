f1d1ef05292fa3acf9fa50170674b42c653bad63
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2016-01-12 14:01:56 -0200

    Huge refactor. Added sass.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,27 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var browserSync = require("browser-sync").create();
 
 // Generate .css file
 gulp.task("styles", function () {
   gulp
-    .src("styles/scss/**/*.scss")
-    .pipe(sass().on("error"), sasss.logError)
-    .pipe(gulp.dest(".styles/css/"));
+    .src("./styles/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./dist/"))
+    .pipe(browserSync.stream());
+});
+
+// Browser Sync setup
+gulp.task("browser-sync", function () {
+  browserSync.init({
+    server: "./",
+    logFileChanges: false
+  });
 });
 
 // Watch task
-gulp.task("watch", function () {
-  gulp
-    .watch("scss/**/*.scss", ["styles"]);
+gulp.task("serve", ["styles", "browser-sync"], function () {
+
+  gulp.watch("styles/**/*.scss", ["styles"]);
+  gulp.watch("*.html").on('change', browserSync.reload);
 });
