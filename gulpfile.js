d98373c9e4a03b355dbd8f10019154593f1aaabd
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2016-02-08 23:59:50 -0200

    Huge refactoring.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
 var browserSync = require("browser-sync").create();
 
 // Generate .css file
@@ -8,6 +10,18 @@ gulp.task("styles", function () {
     .src("./styles/**/*.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest("./dist/"))
+    .pipe(browserSync.stream());
+});
+
+// Minify javascript files
+gulp.task("compress", function () {
+  gulp
+    .src("js/*.js")
+    .pipe(uglify())
+    .pipe(rename({
+      basename: "bundle"
+    }))
+    .pipe(gulp.dest("./dist/js/"))
     .pipe(browserSync.stream());
 });
 
@@ -20,8 +34,8 @@ gulp.task("browser-sync", function () {
 });
 
 // Watch task
-gulp.task("serve", ["styles", "browser-sync"], function () {
-
+gulp.task("serve", ["styles", "compress", "browser-sync"], function () {
   gulp.watch("styles/**/*.scss", ["styles"]);
-  gulp.watch("*.html").on('change', browserSync.reload);
+  gulp.watch("js/**/*.js", ["compress"]);
+  gulp.watch("*.html").on("change", browserSync.reload);
 });
--- js/app.js
+++ js/app.js
@@ -1,4 +1,39 @@
 $("document").ready(function() {
-  var $menu-button = $("#menu-button");
-  
+  handleShowHideSidebar();
+  handleSideBarClick();
 });
+
+function handleShowHideSidebar () {
+  var $menuButton = $("#menu-button i"),
+      show = "animated slideInLeft",
+      hide = "animated slideOutLeft";
+
+  $menuButton.on("click", function () {
+    var $sideBar = $("#sidebar");
+
+    if ($sideBar.hasClass("slideInLeft")) {
+      $sideBar
+        .removeClass(show)
+        .addClass(hide)
+        .removeClass("hidden");
+    } else {
+      $sideBar
+        .removeClass(hide)
+        .addClass(show)
+        .removeClass("hidden");
+    }
+  });
+}
+
+function handleSideBarClick () {
+  $("#sidebar li a").on("click", function () {
+    var href = $(this).attr("href");
+    $("html, body").animate({
+      scrollTop: $(href).offset().top
+    }, 600);
+    $("#sidebar")
+      .removeClass("animated slideInLeft")
+      .addClass("animated slideOutLeft");
+    return false;
+  });
+}
